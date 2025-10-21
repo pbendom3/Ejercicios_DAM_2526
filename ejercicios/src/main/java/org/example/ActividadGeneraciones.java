6033fea3e3791f29200702601443dd3c4926cbbc
A700\Patricia <p.benaventedomenec@edu.gva.es>
2025-10-21 15:57:59 +0200

    ++

--- ejercicios/src/main/java/org/example/ActividadGeneraciones.java
+++ ejercicios/src/main/java/org/example/ActividadGeneraciones.java
@@ -33,42 +33,42 @@ public class ActividadGeneraciones {
 
         if (seguir){
 
-            if (modo==1){
+            switch(modo){
+                case 1:
+                    // el programa deberá pedir el año de nacimiento de la persona (String).
+                    // Después, el programa debe convertir a entero (int) el valor introducido y comprobar si es un valor válido (año >=1900 y año <= año_actual).
 
-                // el programa deberá pedir el año de nacimiento de la persona (String).
-                // Después, el programa debe convertir a entero (int) el valor introducido y comprobar si es un valor válido (año >=1900 y año <= año_actual).
+                    System.out.println("Introduce tu año de nacimiento: ");
+                    String anyo_nacimiento = teclado.next();
 
-                System.out.println("Introduce tu año de nacimiento: ");
-                String anyo_nacimiento = teclado.next();
+                    try{
+                        anyo_nacimiento_int = Integer.parseInt(anyo_nacimiento);
+                    }catch (NumberFormatException e){
+                        System.out.println("El formato no es numérico.");
+                        return;
+                    }
+                    break;
+                case 2:
+                    int edad = 0;
 
-                try{
-                    anyo_nacimiento_int = Integer.parseInt(anyo_nacimiento);
-                }catch (NumberFormatException e){
-                    System.out.println("El formato no es numérico.");
-                    return;
-                }
+                    System.out.println("Introduce la edad: ");
+                    if (teclado.hasNextInt()){
+                        edad = teclado.nextInt();
+                    }else{
+                        System.out.println("La edad no tiene formato correcto (numérico).");
+                        return;
+                    }
 
-            } else if (modo==2) {
-                int edad = 0;
-
-                System.out.println("Introduce la edad: ");
-                if (teclado.hasNextInt()){
-                    edad = teclado.nextInt();
-                }else{
-                    System.out.println("La edad no tiene formato correcto (numérico).");
-                    return;
-                }
-
-                if (edad>=0){
-                    anyo_nacimiento_int = anyo_actual-edad;
-                }else{
-                    System.out.println("La edad no es correcta.");
-                    return;
-                }
-
-            }else {
-                System.out.println("El modo introducido no es correcto.");
-                return;
+                    if (edad>=0){
+                        anyo_nacimiento_int = anyo_actual-edad;
+                    }else{
+                        System.out.println("La edad no es correcta.");
+                        return;
+                    }
+                    break;
+                default:
+                    System.out.println("El modo introducido no es correcto.");
+                  break;
             }
 
             if (anyo_nacimiento_int>=ANYO_MINIMO && anyo_nacimiento_int<=anyo_actual){
--- ejercicios/src/main/java/org/example/CosasHasNextInt.java
+++ ejercicios/src/main/java/org/example/CosasHasNextInt.java
@@ -6,13 +6,38 @@ public class CosasHasNextInt {
 
     static void main() {
 
-        Scanner teclado = new Scanner(System.in);
+//        Scanner teclado = new Scanner(System.in);
+//
+//        if (teclado.hasNextInt()){
+//            int numero = teclado.nextInt();
+//            System.out.println(numero);
+//        }else{
+//            System.out.println("Formato incorrecto. No es un número.");
+//        }
 
-        if (teclado.hasNextInt()){
-            int numero = teclado.nextInt();
-            System.out.println(numero);
-        }else{
-            System.out.println("Formato incorrecto. No es un número.");
+        int numero = 1;
+        int caracter = 6;
+        String palabra = "Patricia";
+
+        switch(caracter){
+
+            case 1:
+                System.out.println("Lunes");
+            case 2:
+                System.out.println("Martes");
+            case 3:
+                System.out.println("Miércoles");
+                System.out.println("Entre semana");
+                break;
+            case 6:
+                System.out.println("Sábado");
+            case 7:
+                System.out.println("Domingo");
+                System.out.println("Fin de semana");
+                break;
+            default:
+                System.out.println("No sé el nombre");
+                break;
         }
 
 
